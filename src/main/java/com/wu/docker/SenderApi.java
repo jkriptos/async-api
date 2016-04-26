298764f00155b0bce77ca74a88ba4fbb96bf9997
jasong <jkriptos+github@gmail.com>
2016-04-26 11:05:21 -0600

    initial commit

---
+++ src/main/java/com/wu/docker/SenderApi.java
@@ -0,0 +1,17 @@
+package com.wu.docker;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.Date;
+
+@Path("senders")
+public class SenderApi {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getIt() {
+        return "{ firstname: 'Joaquin', lastname: 'sabina', age: 32, rating: 4.3 }";
+    }
+}
